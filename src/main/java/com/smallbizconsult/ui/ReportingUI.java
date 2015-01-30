6309b2a483c9e0a1ea78803f3fcfbfb527205c64
ngubenis <swelihle.ngubeni@barclays.com>
2015-01-30 16:17:46 +0200

    dfsdfdsfsd dffsdfsdf sdffsdf

--- src/main/java/com/smallbizconsult/ui/ReportingUI.java
+++ src/main/java/com/smallbizconsult/ui/ReportingUI.java
@@ -3,36 +3,41 @@ package com.smallbizconsult.ui;
 import com.smallbizconsult.model.ModelA;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.data.Item;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.dussan.vaadin.dcharts.DCharts;
+import org.dussan.vaadin.dcharts.base.elements.Trendline;
 import org.dussan.vaadin.dcharts.base.elements.XYaxis;
+import org.dussan.vaadin.dcharts.base.elements.XYseries;
+import org.dussan.vaadin.dcharts.base.renderers.MarkerRenderer;
 import org.dussan.vaadin.dcharts.data.DataSeries;
 import org.dussan.vaadin.dcharts.data.Ticks;
+import org.dussan.vaadin.dcharts.metadata.TooltipAxes;
+import org.dussan.vaadin.dcharts.metadata.XYaxes;
+import org.dussan.vaadin.dcharts.metadata.Yaxes;
+import org.dussan.vaadin.dcharts.metadata.locations.TooltipLocations;
 import org.dussan.vaadin.dcharts.metadata.renderers.AxisRenderers;
+import org.dussan.vaadin.dcharts.metadata.renderers.LabelRenderers;
 import org.dussan.vaadin.dcharts.metadata.renderers.SeriesRenderers;
-import org.dussan.vaadin.dcharts.options.Axes;
-import org.dussan.vaadin.dcharts.options.Highlighter;
-import org.dussan.vaadin.dcharts.options.Options;
-import org.dussan.vaadin.dcharts.options.SeriesDefaults;
+import org.dussan.vaadin.dcharts.metadata.styles.MarkerStyles;
+import org.dussan.vaadin.dcharts.options.*;
+import org.dussan.vaadin.dcharts.renderers.axis.LinearAxisRenderer;
+import org.dussan.vaadin.dcharts.renderers.tick.AxisTickRenderer;
 import org.vaadin.spring.VaadinUI;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @VaadinUI
+@Widgetset("com.smallbizconsult.ui.AppWidgetSet")
 public class ReportingUI extends UI {
 
     public static final String fileLocation = "F:\\Downloads\\Sample - Superstore Sales (Excel).xls";
@@ -45,8 +50,18 @@ public class ReportingUI extends UI {
         table.addContainerProperty(ModelA.Headings.OrderDate.caption, Date.class, null);
         table.addContainerProperty(ModelA.Headings.OrderQuantity.caption, Integer.class, null);
         table.addContainerProperty(ModelA.Headings.CustomerName.caption, String.class, null);
+        table.addContainerProperty(ModelA.Headings.ProductName.caption, String.class, null);
+        table.addContainerProperty(ModelA.Headings.Sales.caption, Double.class, null);
+        table.addContainerProperty(ModelA.Headings.Region.caption, String.class, null);
+        table.addContainerProperty(ModelA.Headings.Province.caption, String.class, null);
+        table.addContainerProperty(ModelA.Headings.Profit.caption, Double.class, null);
 
-        for (ModelA model : buildModel()) {
+        List<ModelA> models = buildModel();
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> qData = new ArrayList<>();
+        Map<String, Integer> monthlyQtyMap = new TreeMap<>();
+        Map<String, Integer> yearSalesMap = new TreeMap<>();
+        for (ModelA model : models) {
             Object itemId = table.addItem();
             Item item = table.getItem(itemId);
             item.getItemProperty(ModelA.Headings.RowID.caption).setValue(model.getRowId());
@@ -54,17 +69,65 @@ public class ReportingUI extends UI {
             item.getItemProperty(ModelA.Headings.OrderDate.caption).setValue(model.getOrderDate());
             item.getItemProperty(ModelA.Headings.OrderQuantity.caption).setValue(model.getQuantity());
             item.getItemProperty(ModelA.Headings.CustomerName.caption).setValue(model.getCustomer());
+            item.getItemProperty(ModelA.Headings.ProductName.caption).setValue(model.getProductName());
+            item.getItemProperty(ModelA.Headings.Sales.caption).setValue(model.getSales());
+            item.getItemProperty(ModelA.Headings.Region.caption).setValue(model.getRegion());
+            item.getItemProperty(ModelA.Headings.Province.caption).setValue(model.getProvince());
+            item.getItemProperty(ModelA.Headings.Profit.caption).setValue(model.getProfit());
+
+            String key = model.getCustomer();
+            int qty = model.getQuantity();
+            if (map.containsKey(key)) {
+                qty = map.get(key) + qty;
+            }
+
+            if (map.keySet().size() < 10) {
+                map.put(key, qty);
+            }
+
+            String year = new SimpleDateFormat("yyyy").format(model.getOrderDate());
+            if (yearSalesMap.containsKey(year)) {
+                yearSalesMap.put(year, yearSalesMap.get(year) + model.getQuantity());
+            } else {
+                yearSalesMap.put(year, model.getQuantity());
+            }
+
+            // monthly quantity
+            String month = new SimpleDateFormat("MMM-yyyy").format(model.getOrderDate());
+            Integer mQty = 0;
+            if (!monthlyQtyMap.containsKey(month)) {
+                mQty = model.getQuantity();
+            } else {
+                mQty += monthlyQtyMap.get(month) + model.getQuantity();
+            }
+
+            monthlyQtyMap.put(month, mQty);
         }
 
         VerticalLayout layout = new VerticalLayout();
         layout.addComponent(table);
-        generateCharts(layout);
-        setContent(layout);
+        String[] names = new String[map.keySet().size()];
+
+        Component chart = generateCharts(map.values().toArray(new Integer[map.values().size()]), map.keySet().toArray(names));
+        Component lineChart = drawLineChart(names);
+        Component salesChart = drawMonthlySalesChart(monthlyQtyMap, yearSalesMap);
+        Component monthlySalesChart = monthlySalesChart(yearSalesMap);
+
+        layout.addComponent(chart);
+        layout.addComponent(lineChart);
+        layout.addComponent(salesChart);
+        layout.addComponent(monthlySalesChart);
+
+        TabSheet tabSheet = new TabSheet();
+        tabSheet.addTab(table, "Data", new ThemeResource("WEB-INF/img/report_icon.jpg"));
+        tabSheet.addTab(layout, "Charts", new ThemeResource("WEB-INF/img/icon1.jpg"));
+
+        setContent(tabSheet);
     }
 
-    private void generateCharts(VerticalLayout layout) {
+    private Component generateCharts(Integer[] qData, String[] names) {
         DataSeries dataSeries = new DataSeries()
-                .add(2, 6, 7, 10);
+                .add(qData);
 
         SeriesDefaults seriesDefaults = new SeriesDefaults()
                 .setRenderer(SeriesRenderers.BAR);
@@ -75,7 +138,8 @@ public class ReportingUI extends UI {
                                 .setRenderer(AxisRenderers.CATEGORY)
                                 .setTicks(
                                         new Ticks()
-                                                .add("a", "b", "c", "d")));
+                                                .add(names)));
+//                                                .add("a", "b", "c", "d")));
 
         Highlighter highlighter = new Highlighter()
                 .setShow(false);
@@ -84,13 +148,135 @@ public class ReportingUI extends UI {
                 .setSeriesDefaults(seriesDefaults)
                 .setAxes(axes)
                 .setHighlighter(highlighter);
+//                .setSeries(series);
 
         DCharts chart = new DCharts()
                 .setDataSeries(dataSeries)
-                .setOptions(options);
-//                .show();
+                .setOptions(options)
+                .show();
 
-        layout.addComponent(chart);
+        return chart;
+    }
+
+    private Component drawLineChart(String[] names) {
+        Title title = new Title("Plot With Options");
+
+        AxesDefaults axesDefaults = new AxesDefaults()
+                .setLabelRenderer(LabelRenderers.CANVAS);
+
+        Axes axes = new Axes()
+                .addAxis(
+                        new XYaxis()
+                                .setLabel("Time")
+                                .setPad(0))
+                .addAxis(
+                        new XYaxis(XYaxes.Y)
+                                .setLabel("Quantity"));
+
+        List<XYseries> seriesList = new ArrayList<>();
+        for (int i = 0; i < names.length; i++) {
+            XYseries xYseries = new XYseries().setLabel(names[i]);
+            seriesList.add(xYseries);
+        }
+
+        Series series = new Series();
+        for(XYseries xYseries : seriesList) {
+            series.addSeries(xYseries);
+        }
+
+        Cursor cursor = new Cursor()
+                .setShow(true);
+
+        Options options = new Options()
+                .setAxesDefaults(axesDefaults)
+                .setAxes(axes)
+                .addOption(cursor)
+                .setSeries(series);
+
+        DataSeries dataSeries = new DataSeries()
+                .add(3, 7, 9, 1, 4, 6, 8, 2, 5)
+                .add(1, 3, 6, 9, 12, 15, 10, 8);
+
+        DCharts chart = new DCharts()
+                .setDataSeries(dataSeries)
+                .setOptions(options)
+                .show();
+
+        return chart;
+    }
+
+    private Component drawMonthlySalesChart(Map<String, Integer> map, Map<String, Integer> yMap) {
+        DataSeries dataSeries = new DataSeries();
+        DataSeries subSeries = dataSeries.newSeries();
+        for (String month : map.keySet()) {
+            subSeries.add(month, map.get(month));
+        }
+
+        Axes axes = new Axes()
+                .addAxis(
+                        new XYaxis()
+                                .setRenderer(AxisRenderers.DATE)
+                                .setTickOptions(
+                                        new AxisTickRenderer()
+                                                .setFormatString("%#m/%#d/%y"))
+                                .setNumberTicks(4))
+                .addAxis(
+                        new XYaxis(XYaxes.Y)
+                                .setTickOptions(
+                                        new AxisTickRenderer()
+                                                .setFormatString("$%.2f")));
+
+        Cursor cursor = new Cursor()
+                .setShow(true);
+
+        Options options = new Options()
+//                .addOption(seriesDefaults)
+                .addOption(axes)
+//                .addOption(highlighter)
+                .addOption(cursor);
+
+        DCharts chart = new DCharts()
+                .setDataSeries(dataSeries)
+                .setOptions(options)
+                .show();
+
+        return chart;
+    }
+
+    private Component monthlySalesChart(final Map<String, Integer> yMap) {
+        DataSeries ds = new DataSeries();
+        DataSeries ds1 = ds.newSeries();
+        for (String year : yMap.keySet()) {
+            ds1.add(year, yMap.get(year));
+        }
+
+        Axes axes = new Axes()
+                .addAxis(
+                        new XYaxis()
+                                .setRenderer(AxisRenderers.DATE)
+                                .setTickOptions(
+                                        new AxisTickRenderer()
+                                                .setFormatString("%#m/%#d/%y"))
+                                .setNumberTicks(4))
+                .addAxis(
+                        new XYaxis(XYaxes.Y)
+                                .setTickOptions(
+                                        new AxisTickRenderer()
+                                                .setFormatString("$%.2f")));
+
+        Cursor cursor = new Cursor()
+                .setShow(true);
+
+        Options options = new Options()
+                .addOption(axes)
+                .addOption(cursor);
+
+        DCharts chart = new DCharts()
+                .setDataSeries(ds)
+                .setOptions(options)
+                .show();
+
+        return chart;
     }
 
     public List<ModelA> buildModel() {
